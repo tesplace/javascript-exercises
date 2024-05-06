6a9d3f006dee138b5284d48365f3531826152322
tesplace <161244753+tesplace@users.noreply.github.com>
2024-05-06 22:41:16 +0200

    completes the 3rd and 4th exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
 
+    const newArray = [];
+
+    array.forEach( (elements) => {
+
+        if (!element.includes(elements)) {
+          newArray.push(elements);
+        }
+      });
+
+      return newArray;
+ 
 };
 
 // Do not edit below this line
