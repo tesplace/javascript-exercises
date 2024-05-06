025c276a704bdd61813134a9f8d82784b5e8be6d
tesplace <161244753+tesplace@users.noreply.github.com>
2024-05-06 23:06:11 +0200

    complete the 5th exercise. following math equation has been used.. 1+2+3+...n = n.(n+1)/2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let outcome = " ";
+     
+    if ( typeof(b) != `number` || typeof(a) != `number`) {
+        outcome = "ERROR";
+    } else if ( a < 0 || b < 0) {
+        outcome = "ERROR";
+    } else if (a > b) {
+        outcome = (a*(a+1)/2) - ((b*(b+1)/2)-1);
+    } else {
+        outcome = (b*(b+1)/2) - ((a*(a+1)/2)-1);
+    }
+
+    return outcome
 
 };
 
