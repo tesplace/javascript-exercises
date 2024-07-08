0755f0f87a59967f0ccc565e9f87ff0e57175ade
tesplace <161244753+tesplace@users.noreply.github.com>
2024-07-08 20:53:22 +0200

    Completes the exercises until calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,62 @@
-const add = function() {
+const add = function(a,b) {
+	return Number(a)+Number(b);
+};
+
+const subtract = function(a,b) {
+  return a-b;
+
+};
+
+const sum = function(array) {
+  if(array.length === 0) {
+    return 0;
+  } else if (array.length === 1) {
+    return array[0]
+  } else {
+    let sumAll = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      sumAll += array[i];
+    }
+
+    return sumAll;
+  }
+};
+
+
+const multiply = function(array) {
+  let multi = 1;
+
+    for (let i = 0; i < array.length; i++) {
+      multi *= array[i];
+    }
+
+    return multi;
+};
+
+const power = function(a, b) {
+  let pow = 1;
+
+  for (let i = 1; i<=b; i++) {
+    pow *= a
+  }
 	
+  return pow;
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(a) {
+  let fac = 1; 
 
-const sum = function() {
-	
-};
+  if (a === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i<=a; i++) {
+      fac *= i
+    }
 
-const multiply = function() {
+    return fac;
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
 	
 };
 
