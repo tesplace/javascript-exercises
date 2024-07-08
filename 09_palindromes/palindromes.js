31e09d59b9dca06753430df8eca36a12ff2ad65e
tesplace <161244753+tesplace@users.noreply.github.com>
2024-07-08 21:46:44 +0200

    completes palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let word = string.toLowerCase().replace(/[^\w\']/g,"");
+    
 
+    let newArray = word.split("").reverse().join("");
+    
+
+    return newArray === word;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// spread the given string to an array 
+// find the index of the coma and spaces if there are any and pop them out
+// reverse array
+// rejoin as a new array
+// check if its the same with the given string
