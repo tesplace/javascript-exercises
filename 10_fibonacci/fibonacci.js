27d8fa9a44f369df668808855a14ff62a809fd92
tesplace <161244753+tesplace@users.noreply.github.com>
2024-07-11 21:17:27 +0200

    Completes the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const fib = [1,1];
 
+    if(n == 0) {
+        return 0;
+    } else if ( n > 0) {
+        for (let i = 2 ; i<n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }       
+        return fib[n-1]
+    } else {
+        return "OOPS";
+    }
+
+    
 };
 
 // Do not edit below this line
